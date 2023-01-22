2b1c11c27caf07115396b67cb6ecd26080861b55
Becka McNally <beckamcnally@gmail.com>
2023-01-22 08:14:02 -0800

    questions prompt all the way through

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer
+// TODO: Create a function to initialize app
+function init() {
+const questions = inquirer
   .prompt([
     {
         type: 'input',
         message: 'What is the title of your project?',
         name: 'title',
+
     },
     {
         type: 'input',
@@ -38,7 +41,8 @@ inquirer
         type: 'list',
         message: 'Choose a license',
         name: 'license',
-        choices: []
+        choices: ['None', 'Apache License 2.0', 'GNU General Public LIcense v3.0', 'BSD 2-Cluase "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public LIcense 2.0', 'GNU Affero General Public LIcense v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License v2.1', 'The Unlicense']
+        //got these from Github - had to add to get it to complete
     },
     {
         type: 'input',
@@ -52,10 +56,21 @@ inquirer
     },
   ])
   .then((answers) => {
-    let title = answers.
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.log(error)
-    } 
-  });
+    console.log(answers)
+    writeToFile()
+    });    
+}
+
+//   // TODO: Create a function to write README file
+  function writeToFile(README, data) {
+    fs.writeFile('./changeName.md', answers, 'utf8', (err) => {
+        if (err) throw err;
+        console.log("the file has been saved!");
+    })
+}
+  
+
+
+  
+//   // Function call to initialize app
+  init();
---
+++ resourse/generateMarkdown.js
@@ -0,0 +1,28 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+
+// I have to IMPORT these over 
+
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  // badeges these are going to go in there own section separate from the license section.
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    // So this is going to be the actual license information I can get that from Github. Do i need to reference that I am getting that information from there?
+
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
